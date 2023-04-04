657f79d19183c76461763c7b8359ba24280aeb65
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2023-04-04 16:40:59 +0200

    added nb for hyperparams check

--- unet/utils.py
+++ unet/utils.py
@@ -1,3 +1,7 @@
+import h5py
+import numpy as np
+
+
 def human_format(num):
     magnitude = 0
     while abs(num) >= 1000:
@@ -12,3 +16,54 @@ def human_format(num):
         return '%.1f%s' % (num, 'B')
     else:
         return '%.1f' % num
+
+
+def create_hdf5(images, masks, predicts, diffs, filename=None):
+    compression = "gzip"
+    with h5py.File(filename, "a") as h:
+        # scores = h.create_dataset(name="events/avg_IOU",
+        #                           data=(avg_score,),
+        #                           dtype="float16")
+        # inftime = h.create_dataset(name="events/inference_time",
+        #                            data=(infr_time,),
+        #                            dtype="float16")
+        imset = h.create_dataset(name="events/image",
+                                 data=np.array(images),
+                                 shape=np.array(images).shape,
+                                 dtype=np.array(images).dtype,
+                                 compression=compression
+                                 )
+        imset.attrs.create("CLASS", "IMAGE", dtype="S6")
+        imset.attrs.create("IMAGE_SUBCLASS", "IMAGE_GRAYSCALE", dtype="S16")
+        imset.attrs.create("IMAGE_VERSION", "1.2", dtype="S4")
+        imset.attrs.create("INTERLACE_MODE", "INTERLACE_PIXEL", dtype="S16")
+        mskset = h.create_dataset(name="events/mask",
+                                  data=np.array(masks),
+                                  shape=np.array(masks).shape,
+                                  dtype=np.array(masks).dtype,
+                                  compression=compression
+                                  )
+        mskset.attrs.create("CLASS", "IMAGE", dtype="S6")
+        mskset.attrs.create("IMAGE_SUBCLASS", "IMAGE_GRAYSCALE", dtype="S16")
+        mskset.attrs.create("IMAGE_VERSION", "1.2", dtype="S4")
+        mskset.attrs.create("INTERLACE_MODE", "INTERLACE_PIXEL", dtype="S16")
+        predset = h.create_dataset(name="events/prediction",
+                                   data=np.array(predicts),
+                                   shape=np.array(predicts).shape,
+                                   dtype=np.array(predicts).dtype,
+                                   compression=compression
+                                   )
+        predset.attrs.create("CLASS", "IMAGE", dtype="S6")
+        predset.attrs.create("IMAGE_SUBCLASS", "IMAGE_GRAYSCALE", dtype="S16")
+        predset.attrs.create("IMAGE_VERSION", "1.2", dtype="S4")
+        predset.attrs.create("INTERLACE_MODE", "INTERLACE_PIXEL", dtype="S16")
+        diffset = h.create_dataset(name="events/msk-pred",
+                                   data=np.array(diffs),
+                                   shape=np.array(diffs).shape,
+                                   dtype=np.array(diffs).dtype,
+                                   compression=compression
+                                   )
+        diffset.attrs.create("CLASS", "IMAGE", dtype="S6")
+        diffset.attrs.create("IMAGE_SUBCLASS", "IMAGE_GRAYSCALE", dtype="S16")
+        diffset.attrs.create("IMAGE_VERSION", "1.2", dtype="S4")
+        diffset.attrs.create("INTERLACE_MODE", "INTERLACE_PIXEL", dtype="S16")
