1a82847b5c5930b7033fc4712030ca9ed6db4e40
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2023-04-16 00:58:00 +0200

    created new ml_dataset.py module

--- unet/ml_dataset.py
+++ unet/ml_dataset.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 
 import albumentations as A
-import h5py
 import numpy as np
 from PIL import Image
-from torch import mean as tmean
+from sklearn.model_selection import train_test_split
 from torch import from_numpy
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader, random_split
-
-from .cli.labelme_utils import json_to_mask
+from torch.utils.data import Dataset, DataLoader
 
 
 def get_dataloaders_with_params(params):
@@ -32,33 +29,46 @@ def get_dataloaders_with_params(params):
     num_workers = dataset_params.get("num_workers")
     num_samples = dataset_params.get("num_samples")
 
-    if data_type.lower() == "json":
-        unet_dataset = UNetDataset.from_json_files(data_path, augmentation,
-                                                   mean, std, num_samples)
-    elif data_type.lower() == "hdf5":
-        unet_dataset = UNetDataset.from_hdf5_data(data_path, augmentation,
-                                                  mean, std, num_samples)
-    else:
-        unet_dataset = UNetDataset.from_png_files(data_path, augmentation,
-                                                  mean, std, num_samples)
+    data_split = load_split_dataset(data_path, valid_size=valid_size)
+    img_train, img_valid, msk_train, msk_valid = data_split
 
-    data_dict = split_dataset(unet_dataset, valid_size)
+    train_dataset = UNetDataset(img_train, msk_train, augment=augmentation,
+                                mean=mean, std=std)
+    valid_dataset = UNetDataset(img_train, msk_train, augment=False,
+                                mean=mean, std=std)
+
+    data_dict = {
+        "train": train_dataset,
+        "valid": valid_dataset
+    }
+
     dataloader_dict = create_dataloaders(data_dict, batch_size, num_workers)
     return dataloader_dict
 
 
-def split_dataset(dataset_object, valid_size=0.2):
-    len_dataset = dataset_object.__len__()
-    valid_data_size = int(valid_size * len_dataset)
-    train_data_size = int(len_dataset - valid_data_size)
-    # Split the data into train and valid datasets randomly
-    train_data, valid_data = random_split(dataset_object,
-                                          [train_data_size, valid_data_size])
-    data = {"train": train_data, "valid": valid_data}
-    return data
+def load_split_dataset(data_path, seed=42, valid_size=0.2):
+    img_path = Path(data_path) / "images"
+    msk_path = Path(data_path) / "masks"
+
+    # Get the list of all the ".png" files
+    img_list = [p for p in Path(img_path).rglob("*.png") if p.is_file()]
+    msk_list = [p for p in Path(msk_path).rglob("*.png") if p.is_file()]
+
+    # Read images and masks
+    images = [np.asarray(Image.open(i)) for i in img_list]
+    masks = [np.asarray(Image.open(i)) for i in msk_list]
+
+    # Crop images and masks
+    images = [img[8:72, :] for img in images]
+    masks = [msk[8:72, :] for msk in masks]
+
+    data_split = train_test_split(images, masks,
+                                  test_size=valid_size,
+                                  random_state=seed)
+    return data_split
 
 
-def create_dataloaders(data_dict, batch_size, num_workers=0, shuffle=True):
+def create_dataloaders(data_dict, batch_size, num_workers=0, shuffle=False):
     dataloader_dict = dict()
     for m in data_dict.keys():
         dataloader_dict[m] = DataLoader(data_dict[m],
@@ -68,81 +78,16 @@ def create_dataloaders(data_dict, batch_size, num_workers=0, shuffle=True):
     return dataloader_dict
 
 
-def compute_mean_std(data_path, data_type="png"):
-    if data_type.lower() == "json":
-        dataset = UNetDataset.from_json_files(data_path)
-    elif data_type.lower() == "hdf5":
-        dataset = UNetDataset.from_hdf5_data(data_path)
-    else:
-        dataset = UNetDataset.from_png_files(data_path)
-
-    data_loader = DataLoader(dataset, batch_size=32)
-
-    channel_sum, channel_square_sum, batch_counter = 0, 0, 0
-
-    for imgs, _ in data_loader:
-        channel_sum += tmean(imgs, dim=[0, 2, 3])
-        channel_square_sum += tmean(imgs ** 2, dim=[0, 2, 3])
-
-        batch_counter += 1
-
-    mean = float(channel_sum / batch_counter)
-    std = float((channel_square_sum / batch_counter - mean ** 2) ** 0.5)
-    return mean, std
-
-
 class UNetDataset(Dataset):
-    def __init__(self, images, masks,
-                 augment=False, mean=None, std=None):
+    def __init__(self, images, masks, augment=False,
+                 normalize_mode="min_max", mean=None, std=None):
         self.images = images
         self.masks = masks
         self.augment = augment
+        self.normalize_mode = normalize_mode
         self.mean = [0.] if mean is None else [mean]
         self.std = [1.] if std is None else [std]
-
-    @classmethod
-    def from_hdf5_data(cls, hdf5_file, augment=False, mean=None,
-                       std=None, num_samples=None):
-        hdf5_ds = h5py.File(hdf5_file)["events"]
-        ds_len = len(hdf5_ds["image"])
-        num_samples = num_samples if num_samples else ds_len
-        images = hdf5_ds["image"][:num_samples]
-        masks = hdf5_ds["mask"][:num_samples]
-        return cls(images, masks, augment, mean, std)
-
-    @classmethod
-    def from_json_files(cls, json_path, augment=False, mean=None,
-                        std=None, num_samples=None):
-        json_list = [p for p in Path(json_path).rglob("*.json") if p.is_file()]
-        images, masks = json_to_mask(json_list)
-
-        len_images = len(images)
-        num_samples = num_samples if num_samples else len_images
-        images = images[:num_samples]
-        masks = masks[:num_samples]
-        return cls(images, masks, augment, mean, std)
-
-    @classmethod
-    def from_png_files(cls, data_path, augment=False, mean=None,
-                       std=None, num_samples=None):
-        img_path = Path(data_path) / "images"
-        msk_path = Path(data_path) / "masks"
-
-        # Get the list of all the ".png" files
-        img_list = [p for p in Path(img_path).rglob("*.png") if p.is_file()]
-        msk_list = [p for p in Path(msk_path).rglob("*.png") if p.is_file()]
-
-        # Read images and masks
-        images = [np.asarray(Image.open(i)) for i in img_list]
-        masks = [np.asarray(Image.open(i)) for i in msk_list]
-
-        # Test the model with desired number of samples by passing
-        # "num_samples" argument
-        len_images = len(images)
-        num_samples = num_samples if num_samples else len_images
-        images = images[:num_samples]
-        masks = masks[:num_samples]
-        return cls(images, masks, augment, mean, std)
+        self.max_pix_val = 1.0 if normalize_mode == "min_max" else 255.
 
     @staticmethod
     def min_max_norm(img):
@@ -154,18 +99,23 @@ class UNetDataset(Dataset):
     def __getitem__(self, index):
         img = self.images[index]
         msk = self.masks[index]
-        img = self.min_max_norm(img)
+
+        if self.normalize_mode == "min_max":
+            img = self.min_max_norm(img)
+
         msk = msk / msk.max()  # Make sure mask is binary
         if self.augment:
             compose_obj = A.Compose([
-                A.Rotate(7, border_mode=4, p=0.5),
-                A.HorizontalFlip(p=0.3),
+                # A.Rotate(7, border_mode=4, p=0.5),
                 A.VerticalFlip(p=0.3),
-                A.Normalize(self.mean, self.std, max_pixel_value=1.0)
+                A.RandomBrightness(limit=(-1.0, 1.0), p=0.5),
+                A.Normalize(self.mean, self.std,
+                            max_pixel_value=self.max_pix_val)
             ])
         else:
             compose_obj = A.Compose([
-                A.Normalize(self.mean, self.std, max_pixel_value=1.0)
+                A.Normalize(self.mean, self.std,
+                            max_pixel_value=self.max_pix_val)
             ])
         transformed = compose_obj(image=img, mask=msk)
         img_tensor = from_numpy(transformed["image"])
@@ -173,6 +123,6 @@ class UNetDataset(Dataset):
         # Add an extra channel [H, W] --> [1, H, W]
         img_tensor = img_tensor.unsqueeze(0)
         # Apply padding to the images and masks (250 --> 256)
-        padded_img = F.pad(img_tensor, (3, 3, 0, 0), mode='constant', value=0)
-        padded_msk = F.pad(mask_tensor, (3, 3, 0, 0), mode='constant', value=0)
+        padded_img = F.pad(img_tensor, (3, 3, 0, 0), value=0)
+        padded_msk = F.pad(mask_tensor, (3, 3, 0, 0), value=0)
         return padded_img, padded_msk
