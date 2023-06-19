7bbdd97fb3dd111b68ab632bfa1ebb5b21e408b6
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2023-06-19 11:45:54 +0200

    feat: add module to run multiple slurm jobs

---
+++ slurm_template/train_hpc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -l
+#SBATCH -o {{PATH_OUT}}/{{EXP_NAME}}/out.%j.log
+#SBATCH -e {{PATH_OUT}}/{{EXP_NAME}}/err.%j.log
+#SBATCH -D ./
+#SBATCH -J {{JOB_NAME}}
+# We need a GPU node
+#SBATCH --partition=gpu
+#SBATCH --constraint="gpu"
+# request one GPU for model training
+#SBATCH --gres=gpu:1
+# We only need one node
+#SBATCH --nodes=1
+# Request one CPU per task
+# We actually need all the CPUs we can get due to this issue 39 I think:
+# https://gitlab.gwdg.de/maximilian.schloegel/dcml/-/issues/39
+#SBATCH --cpus-per-task=16
+# This is probably redundant, but we only have one task!
+#SBATCH --ntasks-per-node=1
+# Initial tests showed about ~2:30h with bloody_bunny
+#SBATCH --time={{MAX_TIME}}
+# memory required per node. Default units are megabytes.
+#SBATCH --mem={{MAX_MEM}}
+#SBATCH --mail-type=all
+#SBATCH --mail-user={{MAIL_ID}}
+
+
+module purge
+module load anaconda/3/2021.11
+module load cuda/11.2
+#Pytorch
+module load pytorch/gpu-cuda-11.2/1.9.0
+
+pip install virtualenv
+
+## Remove existing venv (if it exists)
+#if [ -d "venv" ]; then
+#    rm -rf venv
+#fi
+# Create and activate a new venv
+virtualenv --system-site-packages venv --python=python3.9.7
+. venv/bin/activate
+
+pip install albumentations
+pip install h5py
+
+srun python -m unet --params_path "{{PARAMS_PATH}}"
+
+# Remove existing venv
+if [ -d "venv" ]; then
+    rm -rf venv
+fi
---
+++ unet/slurm_handler.py
@@ -0,0 +1,84 @@
+import itertools
+from pathlib import Path
+import subprocess as sp
+
+import yaml
+
+params_path = Path(__file__).parents[1] / "params" / "unet_params.yaml"
+bash_path = Path(__file__).parents[1] / "slurm_template" / "train_hpc.sh"
+
+
+def get_exp_name(params):
+    path1 = "unet_tune_d{{depth}}_f{{filters}}_dr{{dropout}}_aug" \
+            "{{augmentation}}_b{{batch_size}}_alpha{{alpha}}_gamma{{gamma}}_" \
+            "lr{{learn_rate}}"
+    path2 = "unet_aug{{augmentation}}_b{{batch_size}}_alpha{{alpha}}" \
+            "_gamma{{gamma}}_lr{{learn_rate}}"
+
+    req_params = {**params.get("model"), **params.get("dataset"),
+                  **params.get("criterion"), **params.get("optimizer")}
+
+    if "depth" in req_params.keys():
+        for key, val in req_params.items():
+            path1 = path1.replace("{{" + key + "}}", str(val))
+        return Path(path1)
+    else:
+        for key, val in req_params.items():
+            path2 = path2.replace("{{" + key + "}}", str(val))
+        return Path(path2)
+
+
+def create_params_combinations(original_dict):
+    dict_combinations = []
+    # Generate all possible combinations of the values in
+    # each nested dictionary
+    combinations = []
+    for o_key, o_value in original_dict.items():
+        if o_key != "hpc_params":
+            params = [v if isinstance(v, list) else [v] for v in
+                      o_value.values()]
+            combinations.append(itertools.product(*params))
+
+    for combo in itertools.product(*combinations):
+        new_dict = {}
+        for i, (dict_key, dict_value) in enumerate(original_dict.items()):
+            if dict_key != "hpc_params":
+                nested_dict = {}
+                for j, (key, value) in enumerate(dict_value.items()):
+                    nested_dict[key] = combo[i][j]
+                new_dict[dict_key] = nested_dict
+        dict_combinations.append(new_dict)
+    return dict_combinations
+
+
+params = yaml.safe_load(open(params_path))
+slurm_file = bash_path.read_text()
+experiment_dicts = create_params_combinations(params)
+
+for exp_dict in experiment_dicts:
+    path_out = params["others"]["path_out"][0]
+    exp_name = get_exp_name(exp_dict)
+    exp_path = Path(path_out) / exp_name
+    exp_path.mkdir(parents=True, exist_ok=True)
+    slurm_path = exp_path / "job.sh"
+    params_path = exp_path / "params.yaml"
+    hpc_job_dict = {
+        "EXP_NAME": exp_name,
+        "PATH_OUT": path_out,
+        "JOB_NAME": exp_name,
+        "MAIL_ID": params["hpc_params"]["mail_id"],
+        "MAX_MEM": params["hpc_params"]["max_mem"],
+        "MAX_TIME": params["hpc_params"]["max_time"],
+        "PARAMS_PATH": params_path
+    }
+    for k, v in hpc_job_dict.items():
+        slurm_file = slurm_file.replace("{{" + k + "}}", str(v))
+    slurm_path.write_text(slurm_file)
+
+    with open(params_path, 'w') as file:
+        # Update path_out in params.yaml with experiment name
+        exp_dict["others"]["path_out"] = str(exp_path)
+        yaml.dump(exp_dict, file, sort_keys=False)
+
+    ret = sp.check_output(f'sbatch {slurm_path}', shell=True)
+
