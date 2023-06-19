24745be43914029aab18c204561e43873a7b5081
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2023-06-19 11:26:21 +0200

    enh: update inference script

--- unet/ml_inferece.py
+++ unet/ml_inferece.py
@@ -9,20 +9,27 @@ from .cli.cli_inference import load_model
 from .ml_metrics import IoUCoeff, DiceCoeff
 from .ml_dataset import *
 
-data_path = "data/training_testing_set_w_beads/testing"
-
-use_cuda = True
-mean = [0.486]
-std = [0.115]
-bsize = 16
-min_max = False
+data_path = Path(__file__).parents[1] / "data/training_testing_set_w_beads/testing"
 
 
-def inference(model_path, results_path):
-    out_path = results_path / "test_results"
+def inference(model_path, results_path, min_max=True, use_cuda=True,
+              bsize=8, save_results=False):
+    if min_max:
+        mean = [0.6663]
+        std = [0.1742]
+    else:
+        mean = [0.486]
+        std = [0.115]
+
+    if use_cuda:
+        out_path = results_path / "test_results_gpu"
+    else:
+        out_path = results_path / "test_results_cpu"
+
     out_path.mkdir(parents=True, exist_ok=True)
 
-    images, masks = read_suffle_data(data_path)
+    img_files, msk_files = get_data_files(data_path, shuffle=False)
+    images, masks = read_data(img_files, msk_files)
     images, masks = crop_data(images, masks)
 
     test_dataset = UNetDataset(images, masks, min_max=min_max,
@@ -34,7 +41,7 @@ def inference(model_path, results_path):
                                     num_workers=0)["test"]
 
     metric1 = IoUCoeff()
-    metric2 = DiceCoeff()
+    metric2 = DiceCoeff(sample_wise=True)
 
     unet = load_model(model_path, use_cuda=use_cuda)
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -54,63 +61,75 @@ def inference(model_path, results_path):
         target_list.append(lbl_batch)
         image_list.append(img_batch)
 
+    inference_time = time.time() - tik
+    inf_time_per_img = inference_time / test_dataset.__len__()
+    if use_cuda:
+        print("Inference time (gpu)/image:", inf_time_per_img)
+    else:
+        print("Inference time (cpu)/image:", inf_time_per_img)
+
     image_torch = torch.cat(image_list, dim=0)
     predict_torch = torch.cat(predict_list, dim=0)
     target_torch = torch.cat(target_list, dim=0)
 
     iou_scores = metric1(predict_torch, target_torch)
     dice_scores = metric2(predict_torch, target_torch)
-    iou_avg = float(iou_scores.detach().cpu().mean())
-    dice_avg = float(dice_scores.detach().cpu().mean())
+    iou_scores = iou_scores.detach().cpu().numpy()
+    dice_scores = dice_scores.detach().cpu().numpy()
 
-    inference_time = time.time() - tik
-    inference_time_per_img = inference_time / test_dataset.__len__()
-
-    image_numpy = image_torch.squeeze(1).detach().cpu().numpy()
-    mask_numpy = target_torch.squeeze(1).detach().cpu().numpy()
-    predict_numpy = torch.sigmoid(predict_torch).squeeze(
-        1).detach().cpu().numpy()
-
-    for n in range(len(image_numpy)):
-        img = image_numpy[n]
-        msk = mask_numpy[n]
-        pred = predict_numpy[n]
-        dice = iou_scores[n]
-        iou = dice_scores[n]
-
-        pred_cnt = find_contours(pred, 0.8)
-        msk_cnt = find_contours(msk, 0.8)
-
-        fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 8))
-
-        axs[0, 0].imshow(img, 'gray')
-        axs[0, 0].axis('off')
-        axs[0, 1].imshow(msk, 'gray')
-        axs[0, 1].set_title("Ground truth")
-        axs[0, 1].axis('off')
-
-        axs[0, 2].imshow(pred, 'gray')
-        axs[0, 2].set_title(f"Pred (IoU: {iou:.3f}, Dice: {dice:.3f})")
-        axs[0, 2].axis('off')
-        gs = axs[1, 1].get_gridspec()
-
-        # remove the underlying axes
-        for ax in axs[1, :]:
-            ax.remove()
-        axbig = fig.add_subplot(gs[1, :])
-        axbig.imshow(img, "gray")
-        axbig.axis('off')
-        for pc in pred_cnt:
-            axbig.plot(pc[:, 1], pc[:, 0], "r", linewidth=2)
-        for mc in msk_cnt:
-            axbig.plot(mc[:, 1], mc[:, 0], "g", linewidth=2)
-
-        red_patch = mpatches.Patch(color='red', label='Prediction')
-        green_patch = mpatches.Patch(color='green', label='Ground Truth')
-        plt.legend(handles=[red_patch, green_patch])
-
+    if save_results:
+        fig = plt.figure(figsize=(8, 4))
+        plt.hist(dice_scores, bins=50, label="Dice Scores")
+        plt.legend()
         fig.tight_layout()
-        fig.savefig(out_path / f"pred{n + 1}.png")
+        fig.savefig(out_path / "dice_scores_dis.png")
         plt.close()
 
-    return test_dataset.__len__(), inference_time_per_img, iou_avg, dice_avg
+        image_numpy = image_torch.squeeze(1).detach().cpu().numpy()
+        mask_numpy = target_torch.squeeze(1).detach().cpu().numpy()
+        predict_numpy = torch.sigmoid(predict_torch).squeeze(
+            1).detach().cpu().numpy()
+
+        for n in range(len(image_numpy)):
+            img = image_numpy[n]
+            msk = mask_numpy[n]
+            pred = predict_numpy[n]
+            dice = iou_scores[n]
+            iou = dice_scores[n]
+
+            pred_cnt = find_contours(pred, 0.8)
+            msk_cnt = find_contours(msk, 0.8)
+
+            fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 8))
+
+            axs[0, 0].imshow(img, 'gray')
+            axs[0, 0].axis('off')
+            axs[0, 1].imshow(msk, 'gray')
+            axs[0, 1].set_title("Ground truth")
+            axs[0, 1].axis('off')
+
+            axs[0, 2].imshow(pred, 'gray')
+            axs[0, 2].set_title(f"Pred (IoU: {iou:.3f}, Dice: {dice:.3f})")
+            axs[0, 2].axis('off')
+            gs = axs[1, 1].get_gridspec()
+
+            # remove the underlying axes
+            for ax in axs[1, :]:
+                ax.remove()
+            axbig = fig.add_subplot(gs[1, :])
+            axbig.imshow(img, "gray")
+            axbig.axis('off')
+            for pc in pred_cnt:
+                axbig.plot(pc[:, 1], pc[:, 0], "r", linewidth=2)
+            for mc in msk_cnt:
+                axbig.plot(mc[:, 1], mc[:, 0], "g", linewidth=2)
+
+            red_patch = mpatches.Patch(color='red', label='Prediction')
+            green_patch = mpatches.Patch(color='green', label='Ground Truth')
+            plt.legend(handles=[red_patch, green_patch])
+
+            fig.tight_layout()
+            fig.savefig(out_path / f"pred{n + 1}.png")
+            plt.close()
+
+    return test_dataset.__len__(), inf_time_per_img, iou_scores, dice_scores
