34e88daf48c4701f1ecc75f3b92023ae927cf1a0
ralajan <raghava.alajangi95@gmail.com>
2024-08-20 17:34:34 +0200

    enh: add test for cli

--- tests/helper_methods.py
+++ tests/helper_methods.py
@@ -2,56 +2,6 @@ import pathlib
 import tempfile
 import zipfile
 
-import torch
-
-
-def get_data_dir():
-    return pathlib.Path(__file__).parent / "data"
-
-
-def get_test_tensors():
-    # Create a test predict tensor
-    predict = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
-                            1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8,
-                            1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7])
-    # Create a test target tensor
-    target = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
-                           1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8,
-                           1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7])
-    # Convert test tensors into 2D tensors as if images and masks
-    predict = predict.view(3, 1, 3, 3)
-    target = target.view(3, 1, 3, 3)
-    return predict, target
-
-
-def find_data(path):
-    """Find.rtdc data files in a directory"""
-    path = pathlib.Path(path)
-    rtdcfiles = [r for r in path.rglob("*.rtdc") if r.is_file()]
-    files = [pathlib.Path(ff) for ff in rtdcfiles]
-    return files
-
-
-def retrieve_data(zip_file):
-    """Extract contents of data zip file and return data files
-    """
-    zpath = pathlib.Path(__file__).resolve().parent / "data" / zip_file
-    # unpack
-    arc = zipfile.ZipFile(str(zpath))
-
-    # extract all files to a temporary directory
-    edest = tempfile.mkdtemp(prefix=zpath.name)
-    arc.extractall(edest)
-
-    # Load RT-DC dataset
-    # find tdms files
-    datafiles = find_data(edest)
-
-    if len(datafiles) == 1:
-        datafiles = datafiles[0]
-
-    return datafiles
-
 
 def retrieve_train_data_path(zip_file):
     zpath = pathlib.Path(__file__).resolve().parent / "data" / zip_file
--- tests/test_train_pipeline.py
+++ tests/test_train_pipeline.py
@@ -1,70 +1,35 @@
-import json
-from pathlib import Path
-
+import pytest
+from pytest_console_scripts import script_runner
 import yaml
 
-from semanticsegmentor.ml_criterions import get_criterion_with_params
-from semanticsegmentor.ml_dataset import get_dataloaders_with_params
-from semanticsegmentor.ml_metrics import get_metric_with_params
-from semanticsegmentor.ml_models import get_model_with_params
-from semanticsegmentor.ml_optimizers import get_optimizer_with_params
-from semanticsegmentor.ml_schedulers import get_scheduler_with_params
-from semanticsegmentor.ml_trainer import SetupTrainer
 from .helper_methods import retrieve_train_data_path
 
-path_in = retrieve_train_data_path("test_dataset.zip") / "dataset"
-params_path = Path(__file__).parent / "data" / "test_params.yaml"
-params = yaml.safe_load(open(params_path))
 
-dataset_params = {
-    "dataset": {
-        "type": "PNG",
-        "data_path": path_in,
-        "augmentation": False,
-        "valid_size": 0.2,
-        "batch_size": 4,
-        "mean": [0.67709],
-        "std": [0.13369],
-        "num_workers": 0,
-        "num_samples": None
-    }
-}
+def test_cli(script_runner):
+    """Test the cytosegment command line interface."""
+    path_in = retrieve_train_data_path("test_dataset.zip") / "dataset"
+    path_out = path_in / "output"
+    result = script_runner.run("cytosegment",
+                               f"data.path={str(path_in)}",
+                               f"path_out={str(path_out)}",
+                               # Test only a small model
+                               "model.depth=1", "model.filters=2",
+                               "model.conv_block=single",
+                               # Test only a small number of epochs
+                               "max_epochs=2")
 
+    assert result.success
 
-def test_trainer_and_pipeline():
-    path_out = path_in.with_name("temp_out")
+    log_files = list(path_out.glob("**/*.yaml"))
 
-    model = get_model_with_params(params)
-    dataloaders = get_dataloaders_with_params(dataset_params)
-    criterion = get_criterion_with_params(params)
-    metric = get_metric_with_params(params)
-    optimizer = get_optimizer_with_params(params, model)
-    scheduler = get_scheduler_with_params(params, optimizer)
-
-    max_epochs = params.get("max_epochs")
-    use_cuda = params.get("use_cuda")
-    min_ckp_acc = params.get("min_ckp_acc")
-    early_stop_patience = params.get("early_stop_patience")
-    tensorboard = params.get("tensorboard")
-
-    trainer = SetupTrainer(model,
-                           dataloaders,
-                           criterion,
-                           metric,
-                           optimizer,
-                           scheduler,
-                           max_epochs=max_epochs,
-                           use_cuda=use_cuda,
-                           min_ckp_acc=min_ckp_acc,
-                           early_stop_patience=early_stop_patience,
-                           path_out=path_out,
-                           tensorboard=tensorboard,
-                           init_from_ckp=None)
-    trainer.start_train()
-    test_log_file = [p for p in path_out.glob("**/train_logs.json")][0]
-    with open(test_log_file) as f:
-        data = json.load(f)
-    assert "epochs" in data.keys()
-    assert len(data["epochs"]) == 2
-    assert data["train_samples"] == 8
-    assert data["valid_samples"] == 2
+    for file in log_files:
+        if file.name == "train_logs.yaml":
+            train_logs = yaml.safe_load(open(file))
+            assert train_logs["epochs"] == 2
+            assert train_logs["train_samples"] == 8
+            assert train_logs["valid_samples"] == 2
+            assert train_logs["test_samples"] == 3
+        elif file.name == "run_params.yaml":
+            run_params = yaml.safe_load(open(file))
+            assert run_params["model"]["depth"] == 1
+            assert run_params["model"]["filters"] == 2
